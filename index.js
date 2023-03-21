7c1c4cd7faf66ffd3e8899ef48f6a52c17b584c2
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-03-21 10:24:50 +0300

    Toy problem

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-// Prompt user to enter student's marks
-let marks = prompt("Please enter the student's marks (between 0 and 100):");
-// Convert marks to a number
-marks = Number(marks);
+// // Prompt user to enter student's marks
+// let marks = prompt("Please enter the student's marks (between 0 and 100):");
+// // Convert marks to a number
+// marks = Number(marks);
 
-// Check if marks are within the valid range
-if (marks < 0 || marks > 100) {
-  console.log(window.alert("Invalid marks entered. Please enter a value between 0 and 100."));
-} else {
-  // Assign grade based on marks
-  let grade;
-  if (marks > 79) {
-    grade =window.alert('Your grade is A');
-  } else if (marks >= 60 && marks <= 79) {
-    grade = window.alert('Your grade is B');
-  } else if (marks >= 50 && marks <= 59) {
-    grade = window.alert('Your grade is C');
-  } else if (marks >= 40 && marks <= 49) {
-    grade = window.alert('Your grade is D');
-  } else {
-    grade = window.alert('Your grade is E');
-  }
+// // Check if marks are within the valid range
+// if (marks < 0 || marks > 100) {
+//   console.log(window.alert("Invalid marks entered. Please enter a value between 0 and 100."));
+// } else {
+//   // Assign grade based on marks
+//   let grade;
+//   if (marks > 79) {
+//     grade =window.alert('Your grade is A');
+//   } else if (marks >= 60 && marks <= 79) {
+//     grade = window.alert('Your grade is B');
+//   } else if (marks >= 50 && marks <= 59) {
+//     grade = window.alert('Your grade is C');
+//   } else if (marks >= 40 && marks <= 49) {
+//     grade = window.alert('Your grade is D');
+//   } else {
+//     grade = window.alert('Your grade is E');
+//   }
 
-  // Output the grade
-  console.log(`The student's grade is ${grade}.`);
-}
+//   // Output the grade
+//   console.log(`The student's grade is ${grade}.`);
+// }
