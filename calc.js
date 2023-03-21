8eb27732843f87f2fba363c703194f92957858a4
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-03-21 11:35:55 +0300

    Commpleted code challenge

--- calc.js
+++ calc.js
@@ -1,28 +1,32 @@
-// //Calculate Net Salary
+//Calculate Net Salary
 
-// //Declare variables
-// let basicSalary, benefits, payee, NHIFDeductions, NSSFDeductions, grossSalary, netSalary;
+//Declare variables
+let basicSalary, benefits, payee, NHIFDeductions, NSSFDeductions, grossSalary, netSalary;
 
-// //Prompt user to enter basic salary
-// basicSalary = prompt("Please enter your basic salary:");
+//Prompt user to enter basic salary
+basicSalary = parseFloat(prompt("Please enter your basic salary:"));
 
-// //Prompt user to enter benefits
-// benefits = prompt("Please enter your benefits:");
+//Prompt user to enter benefits
+benefits = parseFloat(prompt("Please enter your benefits:"));
 
-// //Calculate payee (i.e. Tax)
-// payee = (basicSalary + benefits) * 0.25;
+//Calculate payee (i.e. Tax)
+payee = Math.floor((basicSalary + benefits) * 0.25);
 
-// //Calculate NHIF deductions
-// NHIFDeductions = basicSalary * 0.03;
+//Calculate NHIF deductions
+NHIFDeductions = Math.floor(basicSalary * 0.03);
 
-// //Calculate NSSF deductions
-// NSSFDeductions = basicSalary * 0.05;
+//Calculate NSSF deductions
+NSSFDeductions = Math.floor(basicSalary * 0.05);
 
-// //Calculate gross salary
-// grossSalary = basicSalary + benefits;
+//Calculate gross salary
+grossSalary = Math.floor(basicSalary + benefits);
 
-// //Calculate net salary
-// netSalary = grossSalary - payee - NHIFDeductions - NSSFDeductions;
+//Calculate net salary
+netSalary = grossSalary - payee - NHIFDeductions - NSSFDeductions;
 
-// //Display the net salary
-// console.log("Your net salary is: $" + netSalary);
+//Display the net salary
+console.log(window.alert( `Your P.A.Y.E is ${payee}`));
+console.log(window.alert( `Your gross salary is ${grossSalary}`));
+console.log(window.alert( `Your NHIF Deduction is ${NHIFDeductions}`));
+console.log(window.alert( `Your NSSF Deduction is ${NSSFDeductions}`));
+console.log(window.alert(`Your net salary is: Ksh. ${netSalary}`));
--- speed.js
+++ speed.js
@@ -1,14 +1,14 @@
-let speed = parseInt(prompt("Enter the speed of the car: "));
-//let demeritPoints = 0;
+// let speed = parseInt(prompt("Enter the speed of the car: "));
+// //let demeritPoints = 0;
 
-if (speed <= 70) {
-  console.log(window.alert("Ok"));
-} else {
-  let demeritPoints = Math.floor((speed - 70) / 5);
-  console.log(window.alert(`Points ${demeritPoints}`))
-  if (demeritPoints > 12) {
-    console.log(window.alert("License suspended"));
-  } else {
-    console.log(`Demerit points: ${demeritPoints}`);
-  }
-}
+// if (speed <= 70) {
+//   console.log(window.alert("Ok"));
+// } else {
+//   let demeritPoints = Math.floor((speed - 70) / 5);
+//   console.log(window.alert(`Points ${demeritPoints}`))
+//   if (demeritPoints > 12) {
+//     console.log(window.alert("License suspended"));
+//   } else {
+//     console.log(`Demerit points: ${demeritPoints}`);
+//   }
+// }
